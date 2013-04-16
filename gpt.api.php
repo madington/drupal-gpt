7bef9ea218e7c96831b990ece9fee2099085a303
codycraven <cody@cravencode.com>
2013-04-16 15:16:51 -0700

    Updated API to include a responsive size assignment and fixed targeting example.

--- gpt.api.php
+++ gpt.api.php
@@ -35,7 +35,7 @@ function hook_gpt_load_page_options_alter(&$options) {
   // If on a node page.
   if ($node) {
     // Append targeting key value pair of: type to $node->type.
-    $options['targeting']['type'][] = array(
+    $options['targeting']->type[] = array(
       'value' => $node->type,
       'eval' => FALSE,
     );
@@ -48,8 +48,9 @@ function hook_gpt_load_page_options_alter(&$options) {
  * @param array $ad
  *   Associate array containing:
  *   - container: The containing element ID of the ad unit, string.
- *   - size: (conditionally optional) The JSON array size value of the unit. Is
- *     optional only when "outofpage" is true.
+ *   - size: (conditionally optional) stdClass() object listing breakpoints as
+ *     object properties, contains array for sizes, see below for details.
+ *     Optional only when "outofpage" is true.
  *   - targeting, see hook_gpt_ad_page_options_alter()'s $options['targeting']
  *     definition.
  *   - refresh: Whether the ad should be able to refresh, boolean.
@@ -63,7 +64,14 @@ function hook_gpt_load_page_options_alter(&$options) {
 function hook_gpt_ad_solt_settings_alter(&$ad, &$options) {
   // If top ad unit on the homepage alter the ad's size.
   if (drupal_is_front_page() && $ad['container'] == 'ad-manager-ad-top-0') {
-    $ad['size'] = array(array(728,90));
+    // Take over ad size.
+    $ad['size'] = new stdClass();
+    // From browser width 0 through 727 do not display an ad.
+    $ad['size']->{0} = NULL;
+    // From browser width 0 through 969 display a 728x90 ad.
+    $ad['size']->{728} = array(array(728,90));
+    // From browser width 970 and beyond display a 970x90 or 970x250 ad.
+    $ad['size']->{970} = array(array(970,90),array(970,250));
   }
 }
 
